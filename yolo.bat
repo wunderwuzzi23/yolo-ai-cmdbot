9805412c55b7422be0a2b77c9563642b55023d9d
KAuser2094 <kausar2094@gmail.com>
2023-03-06 12:48:13 +0000

    Fix to bat file, accept all words not one

--- yolo.bat
+++ yolo.bat
@@ -3,4 +3,4 @@
 REM Work around to not have to type in python.exe yolo.py every time. 
 REM Put in C:/Windows/System32 to allow command to work anywhere
 
-python.exe yolo-ai-cmdbot/yolo.py %1
+python.exe yolo-ai-cmdbot/yolo.py %*
