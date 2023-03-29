0d83086bafebc11a2b27b704110b5f73930a8cef
Gurkenglas <gurkenglas@hotmail.de>
2023-03-29 21:53:04 +0200

    cut the yaml frill too

--- yolo.py
+++ yolo.py
@@ -4,18 +4,14 @@
 # Copyright (c) 2023 wunderwuzzi23
 # Greetings from Seattle! 
 
-import os, platform, openai, sys, subprocess, distro, yaml, pyperclip, termcolor, colorama
-
-prompt_path = os.path.dirname(os.path.abspath(__file__))
-with open(os.path.join(prompt_path, "yolo.yaml"), 'r') as file:
-  config = yaml.safe_load(file)
+import os, platform, openai, sys, subprocess, distro, pyperclip, termcolor, colorama
 
 colorama.init()
 os_name = platform.system()
 if os_name == "Linux": os_name += distro.name(pretty=True)
 if os_name == "Darwin": os_name += "/macOS"
 shell = os.environ.get("SHELL", "powershell.exe")
-with open(os.path.join(prompt_path, "prompt.txt"), 'r') as file:
+with open(os.path.join( os.path.dirname(os.path.abspath(__file__)), "prompt.txt"), 'r') as file:
   messages = [{"role": "system", "content": file.read().replace("{shell}", shell).replace("{os}", os_name)}]
 
 blather = print if len(sys.argv) == 1 else lambda *args, **kwargs: print(*args[1:], **kwargs)
@@ -25,7 +21,11 @@ try:
   while True:
     messages.append({"role": "user", "content": query})
     try:
-      command = openai.ChatCompletion.create(messages=messages, **config).choices[0].message.content.strip()
+      command = openai.ChatCompletion.create(messages=messages,
+        model = "gpt-3.5-turbo", # If you have access to gpt-4 API already, you can update this.
+        temperature = 0,
+        max_tokens = 500,
+      ).choices[0].message.content.strip()
     except openai.error.AuthenticationError:
       print("Set the environment variable OPENAI_API_KEY=<API-KEY>. Don't know how to do that? I'll tell you... given an API key.", file=sys.stderr)
       sys.exit(1)
