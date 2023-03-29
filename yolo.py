1c95cd48c0cccc7073c42e875cbc715294c892a4
Gurkenglas <gurkenglas@hotmail.de>
2023-03-29 23:12:15 +0200

    figure out what model you can access

--- yolo.py
+++ yolo.py
@@ -12,22 +12,18 @@ if os_name == "Linux": os_name += distro.name(pretty=True)
 if os_name == "Darwin": os_name += "/macOS"
 shell = os.environ.get("SHELL", "powershell.exe")
 messages = [{"role": "system", "content": f"Translate to one line of input for the shell {shell} on {os_name}."}]
-
+try:
+  model = "gpt-4" if any(engine.id == 'gpt-4' for engine in openai.Engine.list().data) else "gpt-3.5-turbo"
+except openai.error.AuthenticationError:
+  print("Set the environment variable OPENAI_API_KEY=<API-KEY>. Don't know how to do that? I'll tell you... given an API key.", file=sys.stderr)
+  sys.exit(1)
 blather = print if len(sys.argv) == 1 else lambda *args, **kwargs: print(*args[1:], **kwargs)
 try:
   blather("Describe a shell command, or Ctrl-C to exit. ==> ", end = '')
   query = input() if len(sys.argv) == 1 else " ".join(sys.argv[1:])
   while True:
     messages.append({"role": "user", "content": query})
-    try:
-      command = openai.ChatCompletion.create(messages=messages,
-        model = "gpt-3.5-turbo", # If you have access to gpt-4 API already, you can update this.
-        temperature = 0,
-        max_tokens = 500,
-      ).choices[0].message.content.strip()
-    except openai.error.AuthenticationError:
-      print("Set the environment variable OPENAI_API_KEY=<API-KEY>. Don't know how to do that? I'll tell you... given an API key.", file=sys.stderr)
-      sys.exit(1)
+    command = openai.ChatCompletion.create(messages=messages, model=model, temperature=0, max_tokens=500).choices[0].message.content.strip()
     messages.append({"role": "assistant", "content": command})
     blather("Command: ", termcolor.colored(command, 'blue'))
     try:
