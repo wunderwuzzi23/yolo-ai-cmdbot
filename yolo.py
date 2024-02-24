4420ef6f1e608152918589f6c2c9da046cc15114
wunderwuzzi23 <johannr@wunderwuzzi.net>
2024-02-24 07:04:23 -0800

    upgrade to newer openai apis

--- yolo.py
+++ yolo.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 
 # MIT License
-# Copyright (c) 2023 wunderwuzzi23
+# Copyright (c) 2023-2024 wunderwuzzi23
 # Greetings from Seattle! 
 
 import os
 import platform
 import openai
+from openai import OpenAI
 import sys
 import subprocess
 import dotenv 
@@ -50,7 +51,7 @@ def get_full_prompt(user_prompt, shell):
   return prompt
 
 def print_usage():
-  print("Yolo v0.2.1 - by @wunderwuzzi23")
+  print("Yolo v0.3 - by @wunderwuzzi23")
   print()
   print("Usage: yolo [-a] list the current directory information")
   print("Argument: -a: Prompt the user before running the command (only useful when safety is off)")
@@ -78,30 +79,116 @@ def get_os_friendly_name():
     return os_name
 
 
-def set_api_key():
-  # Two options for the user to specify they openai api key.
+def create_client(config):
+  
+  # Three options for the user to specify they openai api key.
   #1. Place a ".env" file in same directory as this with the line:
   #   OPENAI_API_KEY="<yourkey>"
   #   or do `export OPENAI_API_KEY=<yourkey>` before use
   dotenv.load_dotenv()
-  openai.api_key = os.getenv("OPENAI_API_KEY")
+  api_key = os.getenv("OPENAI_API_KEY")
   
+
   #2. Place a ".openai.apikey" in the home directory that holds the line:
   #   <yourkey>
   #   Note: This options will likely be removed in the future
-  if not openai.api_key:  #If statement to avoid "invalid filepath" error
-    home_path = os.path.expanduser("~")    
-    openai.api_key_path = os.path.join(home_path,".openai.apikey")
-
+  if not api_key:  #If statement to avoid "invalid filepath" error
+    home_path = os.path.expanduser("~")   
+    api_key=open(os.path.join(home_path,".openai.apikey"), "r").readline().strip()
+   
   #3. Final option is the key might be in the yolo.yaml config file
   #   openai_apikey: <yourkey>
-  if not openai.api_key:  
-    openai.api_key = config["openai_api_key"]
+  if not api_key:  
+    api_key=config["openai_api_key"]
 
-if __name__ == "__main__":
+  return OpenAI(api_key=api_key)
 
+def call_open_ai(client, query, config, shell):
+  # do we have a prompt from the user?
+  if query == "":
+      print ("No user prompt specified.")
+      sys.exit(-1)
+ 
+  # Load the correct prompt based on Shell and OS and append the user's prompt
+  prompt = get_full_prompt(query, shell)
+
+  # Make the first line also the system prompt
+  system_prompt = prompt[1]
+  #print(prompt)
+
+  # Call the ChatGPT API
+  response = client.chat.completions.create(model=config["model"],
+  messages=[
+      {"role": "system", "content": system_prompt},
+      {"role": "user", "content": prompt}
+  ],
+  temperature=config["temperature"],
+  max_tokens=config["max_tokens"])
+ 
+  return response.choices[0].message.content.strip()
+
+def check_for_issue(response):
+  prefixes = ("sorry", "i'm sorry", "the question is not clear", "i'm", "i am")
+  if response.lower().startswith(prefixes):
+    print(colored("There was an issue: "+response, 'red'))
+    sys.exit(-1)
+
+def check_for_markdown(response):
+  # odd corner case, sometimes ChatCompletion returns markdown
+  if response.count("```",2):
+    print(colored("The proposed command contains markdown, so I did not execute the response directly: \n", 'red')+response)
+    sys.exit(-1)
+
+def missing_posix_display():
+  display = subprocess.check_output("echo $DISPLAY", shell=True)
+  return display == b'\n'
+
+def prompt_user_input(config, ask_flag, response):
+  print("Command: " + colored(response, 'blue'))
+  #print(config["safety"])
+
+  if bool(config["safety"]) == True or ask_flag == True:
+    prompt_text = "Execute command? [Y]es [n]o [m]odify [c]opy to clipboard ==> "
+    if os.name == "posix" and missing_posix_display():
+        prompt_text =  "Execute command? [Y]es [n]o [m]odify ==> "
+    print(prompt_text, end = '')
+    user_input = input()
+    return user_input 
+  
+  if config["safety"] == False:
+     return "Y"
+
+def evaluate_input(client, config, user_input, command, shell, ask_flag):
+  if user_input.upper() == "Y" or user_input == "":
+    if shell == "powershell.exe":
+      subprocess.run([shell, "/c", command], shell=False)  
+    else: 
+      # Unix: /bin/bash /bin/zsh: uses -c both Ubuntu and macOS should work, others might not
+      subprocess.run([shell, "-c", command], shell=False)
+  
+  if user_input.upper() == "M":
+    print("Modify prompt: ", end = '')
+    modded_query = input()
+    modded_response = call_open_ai(client, modded_query, config, shell)
+    check_for_issue(modded_response)
+    check_for_markdown(modded_response)
+    modded_user_input = prompt_user_input(config, ask_flag, modded_response)
+    print()
+    evaluate_input(client, config, modded_user_input, modded_response, shell, ask_flag)
+  
+  if user_input.upper() == "C":
+      if os.name == "posix" and missing_posix_display():
+        return
+      pyperclip.copy(command) 
+      print("Copied command to clipboard.")
+
+
+def main():
+  #Enable color output on Windows using colorama
+  init() 
+  
   config = read_config()
-  set_api_key()
+  client = create_client(config)
 
   # Unix based SHELL (/bin/bash, /bin/zsh), otherwise assuming it's Windows
   shell = os.environ.get("SHELL", "powershell.exe") 
@@ -127,94 +214,18 @@ if __name__ == "__main__":
   arguments = sys.argv[command_start_idx:]
   user_prompt = " ".join(arguments)
 
-def call_open_ai(query):
-  # do we have a prompt from the user?
-  if query == "":
-      print ("No user prompt specified.")
-      sys.exit(-1)
- 
-  # Load the correct prompt based on Shell and OS and append the user's prompt
-  prompt = get_full_prompt(query, shell)
-
-  # Make the first line also the system prompt
-  system_prompt = prompt[1]
-  #print(prompt)
-
-  # Call the ChatGPT API
-  response = openai.ChatCompletion.create(
-    model=config["model"],
-    messages=[
-        {"role": "system", "content": system_prompt},
-        {"role": "user", "content": prompt}
-    ],
-    temperature=config["temperature"],
-    max_tokens=config["max_tokens"],
-  )
- 
-  return response.choices[0].message.content.strip()
+  ## core prompting loop logic
+  res_command = call_open_ai(client, user_prompt, config, shell) 
+  check_for_issue(res_command)
+  check_for_markdown(res_command)
+  user_input = prompt_user_input(config, ask_flag, res_command)
+  print()
+  evaluate_input(client, config, user_input, res_command, shell, ask_flag)
 
 
-#Enable color output on Windows using colorama
-init() 
+if __name__ == "__main__":
+  main()
 
-def check_for_issue(response):
-  prefixes = ("sorry", "i'm sorry", "the question is not clear", "i'm", "i am")
-  if response.lower().startswith(prefixes):
-    print(colored("There was an issue: "+response, 'red'))
-    sys.exit(-1)
-
-def check_for_markdown(response):
-  # odd corner case, sometimes ChatCompletion returns markdown
-  if response.count("```",2):
-    print(colored("The proposed command contains markdown, so I did not execute the response directly: \n", 'red')+response)
-    sys.exit(-1)
-
-def missing_posix_display():
-  display = subprocess.check_output("echo $DISPLAY", shell=True)
-  return display == b'\n'
-
-def prompt_user_input(response):
-  print("Command: " + colored(response, 'blue'))
-  #print(config["safety"])
-
-  if bool(config["safety"]) == True or ask_flag == True:
-    prompt_text = "Execute command? [Y]es [n]o [m]odify [c]opy to clipboard ==> "
-    if os.name == "posix" and missing_posix_display():
-        prompt_text =  "Execute command? [Y]es [n]o [m]odify ==> "
-    print(prompt_text, end = '')
-    user_input = input()
-    return user_input 
   
-  if config["safety"] == False:
-     return "Y"
 
-def evaluate_input(user_input, command):
-  if user_input.upper() == "Y" or user_input == "":
-    if shell == "powershell.exe":
-      subprocess.run([shell, "/c", command], shell=False)  
-    else: 
-      # Unix: /bin/bash /bin/zsh: uses -c both Ubuntu and macOS should work, others might not
-      subprocess.run([shell, "-c", command], shell=False)
-  
-  if user_input.upper() == "M":
-    print("Modify prompt: ", end = '')
-    modded_query = input()
-    modded_response = call_open_ai(modded_query)
-    check_for_issue(modded_response)
-    check_for_markdown(modded_response)
-    modded_user_input = prompt_user_input(modded_response)
-    print()
-    evaluate_input(modded_user_input, modded_response)
-  
-  if user_input.upper() == "C":
-      if os.name == "posix" and missing_posix_display():
-        return
-      pyperclip.copy(command) 
-      print("Copied command to clipboard.")
 
-res_command = call_open_ai(user_prompt) 
-check_for_issue(res_command)
-check_for_markdown(res_command)
-user_input = prompt_user_input(res_command)
-print()
-evaluate_input(user_input, res_command)
